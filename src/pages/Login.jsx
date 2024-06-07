f4b5a5ffe63967fa847c76b332ffcc27e9cdfb4a
SHAHNAJ KHATOON <101393500+Shahnaj1@users.noreply.github.com>
2024-06-07 19:26:17 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,98 @@
+// import axios from "axios";
+// import React, { useContext, useState } from "react";
+// import { toast } from "react-toastify";
+// import { Context } from "../main";
+// import { Link, useNavigate, Navigate } from "react-router-dom";
+
+// const Login = () => {
+//   const { isAuthenticated, setIsAuthenticated } = useContext(Context);
+//   const apiUrl = import.meta.env.VITE_API_URL;
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [confirmPassword, setConfirmPassword] = useState("");
+
+//   const navigateTo = useNavigate();
+
+//   const handleLogin = async (e) => {
+//     e.preventDefault();
+//     try {
+//       await axios
+//         .post(
+//           `${apiUrl}/api/v1/user/login`,
+//           { email, password, confirmPassword, role: "Patient" },
+//           {
+//             withCredentials: true,
+//             headers: { "Content-Type": "application/json" },
+//           }
+//         )
+//         .then((res) => {
+//           toast.success(res.data.message);
+//           setIsAuthenticated(true);
+//           navigateTo("/");
+//           setEmail("");
+//           setPassword("");
+//           setConfirmPassword("");
+//         });
+//     } catch (error) {
+//       toast.error(error.response.data.message);
+//     }
+//   };
+
+//   if (isAuthenticated) {
+//     return <Navigate to={"/"} />;
+//   }
+
+//   return (
+//     <>
+//       <div className="container form-component login-form">
+//         <h2>Sign In</h2>
+//         <p>Please Login To Continue</p>
+//         <p>Welcome back! Your journey continues with a quick login.</p>
+//         <form onSubmit={handleLogin}>
+//           <input
+//             type="text"
+//             placeholder="Email"
+//             value={email}
+//             onChange={(e) => setEmail(e.target.value)}
+//           />
+//           <input
+//             type="password"
+//             placeholder="Password"
+//             value={password}
+//             onChange={(e) => setPassword(e.target.value)}
+//           />
+//           <input
+//             type="password"
+//             placeholder="Confirm Password"
+//             value={confirmPassword}
+//             onChange={(e) => setConfirmPassword(e.target.value)}
+//           />
+//           <div
+//             style={{
+//               gap: "10px",
+//               justifyContent: "flex-end",
+//               flexDirection: "row",
+//             }}
+//           >
+//             <p style={{ marginBottom: 0 }}>Not Registered?</p>
+//             <Link
+//               to={"/register"}
+//               style={{ textDecoration: "none", color: "#271776ca" }}
+//             >
+//               Register Now
+//             </Link>
+//           </div>
+//           <div style={{ justifyContent: "center", alignItems: "center" }}>
+//             <button type="submit">Login</button>
+//           </div>
+//         </form>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Login;
+
 import axios from "axios";
 import React, { useContext, useState } from "react";
 import { toast } from "react-toastify";
@@ -5,8 +100,9 @@ import { Context } from "../main";
 import { Link, useNavigate, Navigate } from "react-router-dom";
 
 const Login = () => {
-  const { isAuthenticated, setIsAuthenticated } = useContext(Context);
   const apiUrl = import.meta.env.VITE_API_URL;
+  const { isAuthenticated, setIsAuthenticated } = useContext(Context);
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
@@ -47,7 +143,9 @@ const Login = () => {
       <div className="container form-component login-form">
         <h2>Sign In</h2>
         <p>Please Login To Continue</p>
-        <p>Welcome back! Your journey continues with a quick login.</p>
+        <p>
+        Welcome back! Your journey continues with a quick login.
+        </p>
         <form onSubmit={handleLogin}>
           <input
             type="text"
